6be6f6be3d57dca19ebadfb80a63bf1555369c85
King-David Eroh <kderoh5@gmail.com>
2022-10-17 12:17:47 -0700

    C programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", str);
+	return (0);
+
+}
